6bf082f6e324706ae9f60bb8163a709f2be33792
sunbc0120 <sunbc0120@gmail.com>
2016-02-05 14:59:16 +1100

    sphere quality

--- 3Dmol/gldraw.js
+++ 3Dmol/gldraw.js
@@ -86,7 +86,7 @@ $3Dmol.GLDraw = (function() {
 
         var nvecs = [];
 
-        var subdivisions = 4; // including the initial 2, eg. 4 => 16 subintervals
+        var subdivisions = 5; // including the initial 2, eg. 4 => 16 subintervals
         var N = Math.pow(2, subdivisions);  // eg. 2**4 = 16 subintervals in total
         var i = 2;  // start with 2 subdivisions already done
         var M = Math.pow(2, i); // 4
@@ -719,7 +719,7 @@ $3Dmol.GLDraw = (function() {
                 normals : []
             };
             // scale quality with radius heuristically
-            var sphereQuality = 1;
+            var sphereQuality = 3;
             var widthSegments = 16 * sphereQuality;
             var heightSegments = 10 * sphereQuality;
             if (radius < 1) {
